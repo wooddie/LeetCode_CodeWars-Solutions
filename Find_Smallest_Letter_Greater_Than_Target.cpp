05b19ca7fc9090f64608965d5861cdbb087d6573
wooddie <n1ght.d1ck1ns@gmail.com>
2026-01-31 15:31:36 +0500

    easy leetcode task completed

---
+++ Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Канапия on 31.01.2026.
+//
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    static char nextGreatestLetter(const std::vector<char> &letters, char target) {
+        int left = 0, right = letters.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return letters[left % letters.size()];
+    }
+};
+
+int main() {
+    std::vector<char> l = {'c', 'f', 'j'};
+    std::cout << Solution::nextGreatestLetter(l, 'a');
+    std::cout << Solution::nextGreatestLetter(l, 'c');
+    l = {'x', 'x', 'y', 'y'};
+    std::cout << Solution::nextGreatestLetter(l, 'z');
+
+    return 0;
+}
